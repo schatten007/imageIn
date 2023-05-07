cf94a5950cd73ade7ffc0a9a35b98fff5a21b8c0
schattan007 <hypergamer951@gmail.com>
2023-05-07 17:54:10 +0500

    Debug API

--- server/services/stability.api.js
+++ server/services/stability.api.js
@@ -1,5 +1,5 @@
 const { apiClient } = require('../config/stability.api');
-
+const axios = require("axios")
 // engine_id = stable-diffusion-v1-5 
 // or        = stable-diffusion-v1
 
@@ -7,12 +7,15 @@ const apiRequest = async(path, key, body = {}, method = "GET") => {
     if (!key) throw new Error("Missing API Key");
 
     const config = {
-        headers: {Authorization: `Bearer ${key}`},
-        data: body
+        headers: {
+            Authorization: `Bearer ${key}`, 
+            Accept: 'application/json'
+        }
     }
 
+
     if(method==="POST"){
-        const response = await apiClient.post(path, config);
+        const response = await apiClient.post(path, body, config);
         if (response.status!=200) throw new Error(`HTTP error! status: ${response.status}`);
 
         return response.data;
@@ -23,6 +26,7 @@ const apiRequest = async(path, key, body = {}, method = "GET") => {
         return response.data;
     }
 }
+
 
 const getBalance = async (key) => {
     try{
@@ -47,7 +51,7 @@ const textToImage = async(key, positivePrompts, config = {}) => {
         const negativePrompts = config.negativePrompts || null;
 
         const body = {
-        textPrompts: [
+        text_prompts: [
             {
                 text: positivePrompts,
                 weight: 0.5
@@ -59,14 +63,14 @@ const textToImage = async(key, positivePrompts, config = {}) => {
         ],
         height: 512,
         width: 512,
-        cfgScale: 7,
+        cfg_scale: 7,
         samples: 1,
         seed: 0,
         steps: 25,
         }
-        
+
+
         const image = await apiRequest(`generation/${config.engine}/text-to-image`, key, body, "POST");
-        console.log(image);
         return image;
     }catch(error){
         throw error;
